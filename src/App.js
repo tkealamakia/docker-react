ae7f72e9d7cf1205e4b4ae460e766a498b2c7971
tkealamakia <tkealamakia@overstock.com>
2019-12-27 06:17:49 -0700

    Fixing markup

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Hello CI on EBS!
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
